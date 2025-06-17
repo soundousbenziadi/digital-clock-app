b66c5f4ace7bb727d8e7955041514b1affd915e7
soundous <bsoundous112@gmail.com>
2025-06-17 13:02:51 +0100

    add functionality finish app

--- src/components/DegitalClock.jsx
+++ src/components/DegitalClock.jsx
@@ -1,10 +1,30 @@
 import { useState, useEffect } from "react"
 
 function DegitalClock() {
+    const [time, setTime] = useState(new Date());
+    useEffect(() => {
+        const intervalID = setInterval(() => {
+            setTime(new Date());
+        }, 1000)
+        return () => {
+            clearInterval(intervalID);
+        }
+    }, [])
+    function formatTime() {
+        let hours = time.getHours();
+        let minutes = time.getMinutes();
+        let seconds = time.getSeconds();
+        let meridium = hours >= 12 ? "PM" : "AM";
+        hours = hours > 12 ? hours - 12 : hours;
+        return `${padZero(hours)} : ${padZero(minutes)} : ${padZero(seconds)} ${meridium}`;
+    }
+    function padZero(number) {
+        return (number < 10 ? "0" : "") + number;
+    }
     return (
         <div className="clock-container">
             <div className="clock">
-                00:00:00
+                <span>{formatTime()}</span>
             </div>
         </div>
     )
