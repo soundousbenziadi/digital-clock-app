322c1f6993e193c311068188008b0633eb343dd7
soundous <bsoundous112@gmail.com>
2025-06-17 12:32:11 +0100

    add styles

---
+++ src/components/DegitalClock.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react"
+
+function DegitalClock() {
+    return (
+        <div className="clock-container">
+            <div className="clock">
+                00:00:00
+            </div>
+        </div>
+    )
+}
+export default DegitalClock
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
+import DegitalClock from "../components/DegitalClock"
 function Home() {
     return (
-        <></>)
+        <>
+            <DegitalClock></DegitalClock>
+        </>)
 }
 export default Home
